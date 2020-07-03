75dca7a72ccbf02c8b1892c329c675d5672bf79f
Vinaykumar Shetty <vinay.shetty0592@gmail.com>
2020-07-03 23:49:31 +0530

    Server website from node

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const confg = require('config');
+const path = require('path');
 
 const api = require('./server/api');
 
@@ -19,6 +20,8 @@ app.use([
 ]);
 
 app.use('/api', api);
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
+app.use('/*', express.static(path.join(__dirname, 'build')));
 
 app.listen(confg.get('PORT'), () => {
   console.log('App Listening on 3001');
