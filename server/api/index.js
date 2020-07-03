d9040544dad677c77a3d53b67c07a54c19d0096d
Vinaykumar Shetty <vinay.shetty0592@gmail.com>
2020-07-03 23:45:38 +0530

    Add express-validator

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,29 @@
 const { Router } = require('express');
+const { validationResult } = require('express-validator');
+const { validationLoginForm } = require('../validators')
 
 const api = Router();
 
-api.post('/login', (request, response) => {
+api.post('/login', validationLoginForm, (request, response) => {
+  const result = validationResult(request);
+  if (!result.isEmpty()) {
+    return response.json({
+      succes: false,
+      message: 'Login failed',
+      data: {
+        errors: result.array()
+      }
+    }, 400);
+  }
+
+  return response.json({
+    succes: true,
+    message: 'Login successful!',
+    data: {}
+  })
+});
+
+api.post('/register', (request, response) => {
   console.log(request.body);
   response.json({
     message: 'Hello World'
