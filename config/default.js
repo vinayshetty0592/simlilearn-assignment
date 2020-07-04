b5f8573a5333c70610b0a3b4a46459aa9f081445
Vinaykumar Shetty <vinay.shetty0592@gmail.com>
2020-07-05 00:24:14 +0530

    Install package and set configs

--- config/default.js
+++ config/default.js
@@ -1,5 +1,11 @@
 module.exports = {
   PORT: 3001,
   DATABASE_URL: process.env.DATABASE_URL,
-  PASSWORD_SALT_LENGTH: process.env.PASSWORD_SALT_LENGTH || 10
+  PASSWORD_SALT_LENGTH: process.env.PASSWORD_SALT_LENGTH || 10,
+  JWT: {
+    EXPIRY: '2 days',
+    SECRET_KEY: process.env.JWT_SECRET_KEY || 'simplilearn'
+  },
+  AUTH_COOKIE_NAME: 'x_access_token',
+  AUTH_HEADER_NAME: 'x-access-token',
 };
