0527e068bf89ceff52b736f0088ba3b405b28833
Vinaykumar Shetty <vinay.shetty0592@gmail.com>
2020-07-05 19:05:57 +0530

    Fix logging issue

--- app.js
+++ app.js
@@ -8,30 +8,30 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const app = express();
 const api = require('./server/api');
 
-const app = express();
+app.use([
+  morgan('combined'),
+  helmet(),
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: false }),
+  cookieParser(),
+  cors({
+    credentials: true,
+    origin: true
+  }),
+]);
+
+app.use('/api', api);
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
+app.use('/*', express.static(path.join(__dirname, 'build')));
 
 const init = async () => {
   try {
-    mongoose.connect(config.get('DATABASE_URL'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, });
+    await mongoose.connect(config.get('DATABASE_URL'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, });
     mongoose.connection.on('error', error => console.error(error));
 
-    app.use([
-      bodyParser.json(),
-      bodyParser.urlencoded({ extended: false }),
-      cors({
-        credentials: true,
-        origin: true
-      }),
-      cookieParser(),
-      helmet(),
-      morgan('combined')
-    ]);
-
-    app.use('/api', api);
-    app.use('/static', express.static(path.join(__dirname, 'build/static')));
-    app.use('/*', express.static(path.join(__dirname, 'build')));
 
     app.listen(config.get('PORT'), () => {
       console.log('App Listening on 3001');
